0f74ca796624ff92da9bb098c38cf823b998d032
Josh Patterson <pattersonjosh15@yahoo.com>
2024-07-17 07:55:06 -0400

    Updated module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,21 +12,6 @@ module passwordmanager {
     requires com.sun.jna.platform;
     requires com.sun.jna;
 
-    exports passwordmanager.backend.local.database;
-    exports passwordmanager.frontend.util;
     exports passwordmanager.app;
-    exports passwordmanager.model;
-    exports passwordmanager.frontend.controller;
-    exports passwordmanager.backend;
-
     opens passwordmanager.frontend.controller to javafx.fxml;
-    opens passwordmanager.app to javafx.fxml;
-    exports passwordmanager.backend.encryption;
-    opens passwordmanager.backend.encryption to javafx.fxml;
-    exports passwordmanager.backend.encryption.windows;
-    opens passwordmanager.backend.encryption.windows to javafx.fxml;
-    exports passwordmanager.backend.encryption.linux;
-    opens passwordmanager.backend.encryption.linux to javafx.fxml;
-    exports passwordmanager.frontend.cache;
-
 }
